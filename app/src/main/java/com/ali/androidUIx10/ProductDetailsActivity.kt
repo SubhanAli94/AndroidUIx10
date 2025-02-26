82b9555cef768258d4075f6c8ed83bc93a78a6fb
SubhanAli94 <subhanali.mbzuai@gmail.com>
2025-02-26 18:11:49 +0400

    Init

--- app/src/main/java/com/ali/androidUIx10/ProductDetailsActivity.kt
+++ app/src/main/java/com/ali/androidUIx10/ProductDetailsActivity.kt
@@ -1,27 +1,21 @@
 package com.ali.androidUIx10
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ProductDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_product_details)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btn = findViewById<Button>(R.id.btn)
-        btn.setOnClickListener {
-            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
         }
     }
 }
